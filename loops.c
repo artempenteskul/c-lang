d5f83ca84e2e601b4045bf21e62d4770498bc834
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-11-04 18:03:46 +0200

    loops program

---
+++ loops.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{   
+    printf("FOR LOOP\n");
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%i\n", i);
+    }
+
+    printf("\n");
+    printf("WHILE LOOP\n");
+
+    int j = 1;
+    while (j <= 10)
+    {
+        printf("%i\n", j);
+        j++;
+    }
+
+    printf("\n");
+    printf("DO WHILE LOOP\n");
+    int k = 1;
+    do
+    {
+        printf("%i\n", k);
+        k++;
+    } while (k <= 10);
+}
