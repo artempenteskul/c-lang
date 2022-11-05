e8f501c07aa10d52a0a8fe7371d12efd1d370cb6
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-11-05 20:55:44 +0200

    arrays

---
+++ ages.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 4;
+    int ages[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        ages[i] = 20;
+    }
+
+    for (int i =0; i < n; i++)
+    {
+        printf("%i\n", ages[i]);
+    }
+}
