38b32d7543f195fc81e39c40712bb7d7ed7bb9df
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-11-11 20:44:09 +0200

    swap with pointers

---
+++ swap2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+void swap(int* a, int* b);
+
+
+int main(void)
+{
+    int x = 1;
+    int y = 2;
+
+    printf("x is %i\n", x);
+    printf("y is %i\n", y);
+    printf("\n");
+    printf("Swapping ...\n");
+    printf("\n");
+
+    swap(&x, &y);
+
+    printf("x is %i\n", x);
+    printf("y is %i\n", y);
+}
+
+void swap(int* a, int* b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
